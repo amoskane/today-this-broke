b50d12f070e7f1d5e74d2bfdd2bfc9102978d77e
Amos Eaton <amieaton@gmail.com>
2021-01-05 23:07:13 -0800

    setup

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+
+export default function Contact() {
+  return (
+    <Layout>
+      <div style={{ color: `teal` }}>
+        <p>Send us a message!</p>
+      </div>
+    </Layout>
+  )
+}
